723a40e10b30d9cf1d28cccb46d15a2562679c63
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2023-09-12 12:38:11 +0200

    Add licecount module

---
+++ src/lucy/licecount.py
@@ -0,0 +1,23 @@
+"""
+Functions for computing the number of lice released from farms
+"""
+
+
+def cleanup_lice():
+    pass
+
+
+def cleanup_temp():
+    pass
+
+
+def cleanup_numfish():
+    pass
+
+
+def merge_lice_fish():
+    pass
+
+
+def compute_nauplii():
+    pass
