83cafccc7851dd7454440d279e3c97922445c4a0
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2023-11-15 09:12:41 +0100

    Select NorKyst dataset from time stamp

--- src/lucy/norkyst.py
+++ src/lucy/norkyst.py
@@ -12,13 +12,6 @@ import numpy as np
 OptionalDataset = typing.Union[xr.Dataset, None]
 
 
-class NorKyst:
-    def __init__(self):
-        """
-        An ocean model archive, created using the NorKyst system
-        """
-
-
 class NorKystDataset:
     def __init__(
             self, fname: str, dset: OptionalDataset = None
@@ -35,6 +28,13 @@ class NorKystDataset:
         self._dset_empty_at_init = (dset is None)
         self._start_date = None
         self._stop_date = None
+
+    @property
+    def fname(self):
+        """
+        File name of dataset
+        """
+        return self._fname
 
     def close(self):
         """
@@ -99,3 +99,75 @@ class NorKystDataset:
             self._getdates()
 
         return self._stop_date
+
+
+class NorKystDataseries:
+    def __init__(self, dsets: typing.Iterable[NorKystDataset]):
+        """
+        An ocean model archive, created using the NorKyst system
+
+        :param dsets: An iterable of datasets in the database
+        """
+        self._dsets = list(dsets)
+        self._grid_dset = self._dsets[0]
+        self._time_index = None
+
+    @staticmethod
+    def from_pattern(pattern: str):
+        """
+        Initialize a database object from file name pattern. It is
+        assumed that the files are ordered by time if they are sorted
+        by alphabetic ordering of the file names.
+
+        :param pattern: A file name glob pattern
+        :return: An initialized database
+        """
+        import glob
+        filenames = sorted(glob.glob(pattern))
+        return NorKystDataseries.from_filenames(filenames)
+
+    @staticmethod
+    def from_filenames(filenames: typing.Iterable[str]):
+        """
+        Initialize a database object from file names. It is
+        assumed that the files are ordered by time.
+
+        :param filenames: File names
+        :return: An initialized database
+        """
+        dsets = [NorKystDataset(fname) for fname in filenames]
+        db = NorKystDataseries(dsets)
+        return db
+
+    def _create_time_index(self):
+        self._time_index = [(d.start_date, d.stop_date) for d in self._dsets]
+
+    def select_time(self, time) -> (NorKystDataset, NorKystDataset):
+        """
+        Return the datasets corresponding to a specific time
+
+        The function always returns a tuple of two datasets, the 'lower' and 'upper'
+        dataset. If the given time is fully contained within a single dataset, this
+        dataset is used as both 'lower' and 'upper' dataset. If the given time falls
+        between two datasets, one will be the 'lower' and the other will be the 'upper'
+        dataset.
+
+        If the given time is outside the dataseries interval, an error is raised.
+
+        :param time: A numpy-compatible datetime
+        :return: A tuple (lower, upper) containing the datasets
+        """
+
+        nptime = np.datetime64(time)
+        if (nptime < self._dsets[0].start_date) or (self._dsets[-1].stop_date < nptime):
+            raise ValueError(f'Time value outside range: {nptime}')
+
+        if self._time_index is None:
+            self._create_time_index()
+
+        start_times, stop_times = np.asarray(self._time_index).T
+        dset_index_lower = np.searchsorted(start_times, nptime, side='right') - 1
+        dset_index_upper = np.searchsorted(stop_times, nptime, side='left')
+        lower = self._dsets[dset_index_lower]
+        upper = self._dsets[dset_index_upper]
+        return lower, upper
--- src/tests/test_norkyst.py
+++ src/tests/test_norkyst.py
@@ -1,31 +1,79 @@
+import pytest
 from lucy import norkyst
-import xarray as xr
 import numpy as np
 
 
 class Test_NorKystDataset_start_date:
     def test_can_interpret_his_file_name(self):
-        dset = xr.Dataset()
         fname = "my_norkyst.nc4_2021020304-2025060708"
-        nrdset = norkyst.NorKystDataset(fname, dset)
+        nrdset = norkyst.NorKystDataset(fname)
         assert nrdset.start_date == np.datetime64('2021-02-03 04:00')
 
     def test_can_interpret_avg_file_name(self):
-        dset = xr.Dataset()
         fname = "my_norkyst.nc4_2021020312"
-        nrdset = norkyst.NorKystDataset(fname, dset)
+        nrdset = norkyst.NorKystDataset(fname)
         assert nrdset.start_date == np.datetime64('2021-02-03 12:00')
 
 
 class Test_NorKystDataset_stop_date:
     def test_can_interpret_his_file_name(self):
-        dset = xr.Dataset()
         fname = "my_norkyst.nc4_2021020304-2025060708"
-        nrdset = norkyst.NorKystDataset(fname, dset)
+        nrdset = norkyst.NorKystDataset(fname)
         assert nrdset.stop_date == np.datetime64('2025-06-07 08:00')
 
     def test_can_interpret_avg_file_name(self):
-        dset = xr.Dataset()
         fname = "my_norkyst.nc4_2021020312"
-        nrdset = norkyst.NorKystDataset(fname, dset)
+        nrdset = norkyst.NorKystDataset(fname)
         assert nrdset.stop_date == np.datetime64('2021-02-03 12:00')
+
+
+class Test_NorKystDataseries_select_time:
+    def test_raises_error_if_outside_range(self):
+        fnames = ["my_norkyst.nc4_2021020304-2025060708"]
+        ds = norkyst.NorKystDataseries.from_filenames(fnames)
+        with pytest.raises(ValueError):
+            ds.select_time("2019-01-01")
+        with pytest.raises(ValueError):
+            ds.select_time("2026-01-01")
+
+    def test_returns_correct_internal_dataset(self):
+        fnames = [
+            "my_norkyst.nc4_2021020301-2021020400",
+            "my_norkyst.nc4_2021020401-2021020500",
+        ]
+        ds = norkyst.NorKystDataseries.from_filenames(fnames)
+        lower, upper = ds.select_time('2021-02-03 02:00')
+        assert lower is upper
+        assert lower.fname == fnames[0]
+
+    def test_returns_correct_between_dataset(self):
+        fnames = [
+            "my_norkyst.nc4_2021020301-2021020400",
+            "my_norkyst.nc4_2021020401-2021020500",
+        ]
+        ds = norkyst.NorKystDataseries.from_filenames(fnames)
+        lower, upper = ds.select_time('2021-02-04 00:30')
+        assert lower.fname == fnames[0]
+        assert upper.fname == fnames[1]
+
+    def test_returns_correct_lower_edge_dataset(self):
+        fnames = [
+            "my_norkyst.nc4_2021020301-2021020400",
+            "my_norkyst.nc4_2021020401-2021020500",
+        ]
+        ds = norkyst.NorKystDataseries.from_filenames(fnames)
+        lower, upper = ds.select_time('2021-02-03 01:00')
+        assert lower.fname == fnames[0]
+        assert upper.fname == fnames[0]
+        assert lower is upper
+
+    def test_returns_correct_upper_edge_dataset(self):
+        fnames = [
+            "my_norkyst.nc4_2021020301-2021020400",
+            "my_norkyst.nc4_2021020401-2021020500",
+        ]
+        ds = norkyst.NorKystDataseries.from_filenames(fnames)
+        lower, upper = ds.select_time('2021-02-04 00:00')
+        assert lower.fname == fnames[0]
+        assert upper.fname == fnames[0]
+        assert lower is upper
